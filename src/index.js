f5b26f62d71df6e40325cda632dc6f2a89794276
Muhammad Muttraf Rasool <muttrafgujjar.787@gmail.com>
2023-02-25 00:07:59 +0500

    Fix index error and remove linters checks

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-/* eslint-disable import/no-cycle */
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-mutable-exports */
-/* eslint-disable no-plusplus */
-// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
 
 const Tasks = document.querySelector('.tasks');
-const Input = document.querySelector('form input');
-const AddBtn = document.querySelector('form button');
-const Span = document.querySelector('span');
 
-// eslint-disable-next-line import/prefer-default-export, prefer-const
-let tasks = [
-  { index: 0, completed: false, description: 'Wash dishes' },
-  { index: 0, completed: false, description: 'Wash dishes' },
+const tasks = [
+  { index: 1, completed: false, description: 'Wash dishes' },
+  { index: 2, completed: false, description: 'Best wishes' },
 ];
 
 function listTasks() {
